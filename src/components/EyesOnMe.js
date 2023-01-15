b1b4a2b57e86bdecb475cf1620f8aed14cff5423
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-01-15 09:43:47 +0300

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function focus(){
+        console.log("Good!")
+    }
+    function blur(){
+        console.log("Hey,Eyes on me!")
+    }
+    return(
+        <button onBlur={blur} onFocus={focus}>
+          EyesOnMe
+        </button>
+    )
+}
+export default EyesOnMe
